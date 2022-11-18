5516e9a255a0380ecfe615ad8633e1ab38e62c5a
SauravKharal1 <kharalsaurav@gmail.com>
2022-11-18 20:24:49 +0000

    Removed the unused 'code' variable

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -4,7 +4,7 @@ public class CurrencyConverter {
 
         public static void main(String[] args) {
 
-                double amount, dollar, pound, code, euro;
+                double amount, dollar, pound, euro;
 
                 DecimalFormat f = new DecimalFormat("##.##");
 
